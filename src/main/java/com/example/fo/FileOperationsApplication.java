88fdd8141785aa57830ee0deb568bd7afc83d70b
marszm <mariusz.szmer@gmail.com>
2021-03-18 15:47:10 +0100

    initial repo for file-operations

---
+++ src/main/java/com/example/fo/FileOperationsApplication.java
@@ -0,0 +1,14 @@
+package com.example.fo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileOperationsApplication.class, args);
+		System.out.println("dsadasda");
+	}
+
+}
---
+++ src/test/java/com/example/fo/FileOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
