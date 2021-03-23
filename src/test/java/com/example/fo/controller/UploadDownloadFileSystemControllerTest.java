2a51e569b956898a8a34eb557032854e3af71c10
marszm <mariusz.szmer@gmail.com>
2021-03-23 23:17:43 +0100

    added integration test for test to upload single file

--- src/test/java/com/example/fo/controller/UploadDownloadFileSystemControllerTest.java
+++ src/test/java/com/example/fo/controller/UploadDownloadFileSystemControllerTest.java
@@ -3,6 +3,7 @@ package com.example.fo.controller;
 import com.example.fo.service.FilerStorageService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,10 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,10 +32,13 @@ class UploadDownloadFileSystemControllerTest {
     @DisplayName("test to upload single file")
     void singleFileUpload() throws Exception {
 
+        Mockito.when(filerStorageService.storeFile(any(MultipartFile.class))).thenReturn("test-file.txt");
+
         MockMultipartFile multipartFile = new MockMultipartFile("multipartFile", "test-file.txt", "text/plain", "aaaa".getBytes());
 
         this.mockMvc.perform(MockMvcRequestBuilders.multipart("/single/upload").file(multipartFile))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(content().json("{\"fileName\":test-file.txt,\"contentType\":\"text/plain\",\"url\":\"http://localhost/download/test-file.txt\"}"));
 
     }
 
